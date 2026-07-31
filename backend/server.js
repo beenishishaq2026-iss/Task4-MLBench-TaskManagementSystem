f690dab5f8d7ba4a870f3f7999059cbdca2b3528
beenish04 <beenishishaq1@gmail.com>
2026-08-01 02:38:21 +0500

    Implement task CRUD APIs with search, filter, sort, and pagination

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.get('/' , (req,res)=> {
 });
 
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
 const PORT= process.env.PORT || 5000;
 app.listen(PORT, ()=>{
